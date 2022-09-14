f51c6831ab4cc113ac19aada3c52aa7ebbb3064c
blakehawkins <blake.hawkins.11@gmail.com>
2022-09-14 12:47:23 +0100

    include subpackage

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup(name="transforms", version="0.3.0", packages=['transforms'], package_dir={'transforms': 'src/transforms'})
+setup(
+  name="transforms",
+  version="0.3.0",
+  packages=['transforms', 'transforms.api'],
+  package_dir={'transforms': 'src/transforms', 'transforms.api': 'src/transforms/api'}
+)
